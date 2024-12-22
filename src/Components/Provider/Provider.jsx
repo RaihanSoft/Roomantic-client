a946ea69ef0e254328865cf3b95a3af04b05c258
Raihan <aboraihan14@gmail.com>
2024-12-22 17:17:38 +0600

    auth provider and navbar added

---
+++ src/Components/Provider/Provider.jsx
@@ -0,0 +1,101 @@
+/* eslint-disable react/prop-types */
+import { createContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import { auth } from "../../Firebase/firebase.config";
+import { GoogleAuthProvider } from "firebase/auth";
+
+export const Context = createContext();
+
+export const Provider = ({ children }) => {
+    const Googleprovider = new GoogleAuthProvider();
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+
+    const [visa, setVisa] = useState({});
+
+
+    // Register
+    const handleRegister = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    };
+
+    // Login
+    const handleLogin = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    };
+
+    // Signout
+    const handleLogOut = () => {
+        signOut(auth);
+    };
+
+    // Google LogIn
+    const handleGoogleLogin = async () => {
+        try {
+            const result = await signInWithPopup(auth, Googleprovider);
+            const userData = result.user;
+            setUser({
+                ...userData,
+                displayName: userData.displayName,
+                email: userData.email,
+                photoURL: userData.photoURL,  
+            });
+        } catch (error) {
+            console.error("Error during Google login:", error);
+        }
+    };
+
+    // Update Profile
+    const manageProfile = (name, image) => {
+        return updateProfile(auth.currentUser, {
+            displayName: name,
+            photoURL: image,
+        }).then(() => {
+           
+            setUser({
+                ...auth.currentUser,
+                displayName: name,
+                photoURL: image,
+            });
+        });
+    };
+
+    // Observer
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+            if (currentUser) {
+                setUser({
+                    ...currentUser,
+                    photoURL: currentUser.photoURL, 
+                });
+            } else {
+                setUser(null);
+            }
+            setLoading(false);
+
+            return () => {
+                unSubscribe();
+            };
+        });
+    }, []);
+
+    const authInfo = {
+        handleRegister,
+        handleLogin,
+        handleLogOut,
+        handleGoogleLogin,
+        manageProfile,
+        user,
+        setUser,
+        loading,
+        visa,
+        setVisa
+    };
+
+    return (
+        <Context.Provider value={authInfo}>
+            {children}
+        </Context.Provider>
+    );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Routes/Routes'
-
+import { Provider } from './Components/Provider/Provider'
+import { HelmetProvider } from 'react-helmet-async'
+import { DarkModeProvider } from './DarkMood/DarkMood'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    
+    <HelmetProvider>
+      <Provider>
+        <DarkModeProvider>
           <RouterProvider router={router} />
-    
-    
+        </DarkModeProvider>
+      </Provider>
+    </HelmetProvider>
   </StrictMode>,
 )
